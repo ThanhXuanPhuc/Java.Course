2b33937aa83e1fabb4d62e6c401ba1aa59d8b79a
ADMIN <ADMIN@PC317>
2024-08-19 10:34:35 +0700

    4

--- tuan2/Tuan2/src/main/java/oop4/HinhTron.java
+++ tuan2/Tuan2/src/main/java/oop4/HinhTron.java
@@ -8,13 +8,39 @@ package oop4;
  *
  * @author ADMIN
  */
-public class HinhTron {
+public class HinhTron extends Object{
+    //Khai bao du lieu
+    private double bankinh;
+    //Dinh nghia cac phuong thuc
+    
+    public HinhTron(){
+        bankinh=1.0;
+    }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public HinhTron(double bankinh) {
+        this.bankinh = bankinh;
+    }
+
+    public double getBankinh() {
+        return bankinh;
+    }
+
+    public void setBankinh(double bankinh) {
+        this.bankinh = bankinh;
+    }
+    public double tinhDienTich(){
+        return Math.PI*bankinh*bankinh;
+    }
+
+   public double tinhChuVi(){
+       return Math.PI*2*bankinh;
+   }
+
+    @Override
+    public String toString() {
+        return "HinhTron{" + "bankinh=" + bankinh + ", Dien tich:" + tinhDienTich() + 
+                        ", Chu vi: " + tinhChuVi() + '}';
     }
     
+   
 }
